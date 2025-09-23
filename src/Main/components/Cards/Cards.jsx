7d3f37638c9f214cca5ac57b0e1f5f0c83561165
Marcela Moura <marcela_moura28@hotmail.com>
2025-09-23 15:51:51 +0200

    adicionando o botao de curtir e lixeira

--- src/Main/components/Cards/Cards.jsx
+++ src/Main/components/Cards/Cards.jsx
@@ -1,40 +1,45 @@
 import ImagePopup from "../Popup/ImagePopup/ImagePopup";
+import coracao from "../../../images/coração.png";
+import lixeira from "../../../images/lixeira.png";
 
-
-export default function Card({card, onOpenPopup}) {
+export default function Card({ card, onOpenPopup }) {
   const { name, link, isLiked } = card;
 
   const imageComponent = {
     title: null,
-    children: <ImagePopup card= {card} />,
-    type: "image"
+    children: <ImagePopup card={card} />,
+    type: "image",
   };
 
   return (
-    <li className="card">
-      <div className="card__image-container">
-        <img className="card__image" src={link} alt={name}
-        onClick= {() => onOpenPopup(imageComponent)} />
+    <li className="element">
+      <div className="element__image-container">
+        <img
+          className="element__image"
+          src={link}
+          alt={name}
+          onClick={() => onOpenPopup(imageComponent)}
+        />
         <button
-          className="card__delete-button"
+          className="element__trash"
           aria-label="Excluir card"
           type="button"
+          style={{ backgroundImage: `url(${lixeira})` }}
         />
       </div>
 
-      <div className="card__description">
-        <h2 className="card__title">{name}</h2>
+      <div className="element__info">
+        <h2 className="element__title">{name}</h2>
         <div className="card__actions">
           <button
-            className={`card__like-button ${isLiked ? "card__like-button_active" : ""}`}
+            className={`element__heart ${isLiked ? "active" : ""}`}
             aria-label="Curtir card"
             type="button"
+            style={{ backgroundImage: `url(${coracao})` }}
           />
-          <span className="card__like-count">0</span>
+          <span className="element__like-count">0</span>
         </div>
       </div>
     </li>
   );
 }
-
-
